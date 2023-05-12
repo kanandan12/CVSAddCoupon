22fbbd4c153fdad35efd0c685da6d25da88e0f45
Kamalakannan Anandan <kamalakannan.anandan@gmail.com>
2023-05-12 12:55:17 -0500

    Java Script Executor Example Program

--- CVSAddCoupon/src/main/java/learning/JavaScriptExecutorExample.java
+++ CVSAddCoupon/src/main/java/learning/JavaScriptExecutorExample.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import io.github.bonigarcia.wdm.WebDriverManager;
